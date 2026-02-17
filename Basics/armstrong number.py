7f73f64194779dbba1bc695d7a13d5b73d9bfef3
Nithyasree Balasubramaniyam <nithyasreebalasubramaniam@gmail.com>
2026-02-18 02:23:38 +0530

    function with armstrong number.py

--- Basics/armstrong number.py
+++ Basics/armstrong number.py
@@ -1,13 +1,16 @@
 #armstrong number or not
-n=int(input())
-TEMP=n
-sum=0
-while(n):
-  num=n%10
-  sum=sum+(num**3)
-  n=n//10
-print('sum=',sum)
-if(sum==TEMP):
-  print('Armstrong number')
-else:
-  print('not Armstrong number')
+def armstrong(n):
+  TEMP=n
+  sum=0
+  while(n):
+    num=n%10
+    sum=sum+(num**3)
+    n=n//10
+  print('sum=',sum)
+  if(sum==TEMP):
+    return ('Armstrong number')
+  else:
+    return ('not Armstrong number')
+
+a=armstrong(153)
+print(a)
