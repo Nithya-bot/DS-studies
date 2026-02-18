5edbca864015a848d302c54b18f1bfb09ad8c3a3
Nithyasree Balasubramaniyam <nithyasreebalasubramaniam@gmail.com>
2026-02-18 23:13:55 +0530

    Create nearest_greater_integer.py

---
+++ Basics/nearest_greater_integer.py
@@ -0,0 +1,15 @@
+#with math function
+import math
+x = float(input())
+print(math.ceil(x))
+
+
+----------------
+#without math function
+
+x=float(input())
+if (x==int(x)):
+  print(int(x))
+else:
+  print(int(x)+1)
+  
