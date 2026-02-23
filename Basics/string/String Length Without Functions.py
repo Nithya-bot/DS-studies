3b1f02be08094689166720446ee6b26c1081fba5
Nithyasree Balasubramaniyam <nithyasreebalasubramaniam@gmail.com>
2026-02-24 00:19:39 +0530

    Create String Length Without Functions.py

---
+++ Basics/string/String Length Without Functions.py
@@ -0,0 +1,17 @@
+# Problem Statement:
+# Given a string S, find its length(including the spaces)without using any pre-defined functions.
+
+
+# Sample Input:
+# codekata
+
+
+# Sample Output:
+# 8
+
+
+S=input()
+sum=0
+for i in S:
+    sum+=1
+print(sum)
