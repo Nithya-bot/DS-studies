5ca875c4876278456582bf598e9c3a3f4b026d18
Nithyasree Balasubramaniyam <nithyasreebalasubramaniam@gmail.com>
2026-02-23 22:52:30 +0530

    Create counting_digits_of_a_number.py

---
+++ Basics/counting_digits_of_a_number.py
@@ -0,0 +1,13 @@
+# Problem Statement:
+# Count the number of digits of a given number N.Size of the integer ranges from 1<N<100000000
+
+
+# Sample Input:
+# 548
+
+
+# Sample Output:
+# 3
+
+N=input().strip()
+print(len(N))
