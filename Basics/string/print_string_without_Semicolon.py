dd03685c9b6a46b51c8b632507d17f520536b15c
Nithyasree Balasubramaniyam <nithyasreebalasubramaniam@gmail.com>
2026-02-23 22:59:17 +0530

    Create print_string_without_Semicolon.py

---
+++ Basics/string/print_string_without_Semicolon.py
@@ -0,0 +1,13 @@
+# Problem Statement:
+# Given a string S, print it without using semicolon in your program.
+
+
+# Sample Input:
+# hello world
+
+
+# Sample Output:
+# hello world
+
+S=input()
+print(S)
