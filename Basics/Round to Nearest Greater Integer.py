89c4a9164ca5e2a6e4a3d293592e586348dabcb5
Nithyasree Balasubramaniyam <nithyasreebalasubramaniam@gmail.com>
2026-04-19 19:54:31 +0530

    Update Round to Nearest Greater Integer.py

--- Basics/Round to Nearest Greater Integer.py
+++ Basics/Round to Nearest Greater Integer.py
@@ -3,3 +3,7 @@ https://www.guvi.in/code-kata/round-to-nearest-greater-integer/
 import math
 K=float(input())
 print(math.ceil(K))
+
+
+
+#method with multiplying by 10 and divide by 10 is also there
